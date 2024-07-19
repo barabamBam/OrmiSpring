13b7c2b21404848619abda0ee1425913bdbc6e97
st-minju <kimminju0115@gmail.com>
2024-07-19 16:04:30 +0900

    :tada: Study Start

---
+++ src/main/java/com/barabam/ormispring/OrmiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.barabam.ormispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barabam/ormispring/OrmiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barabam.ormispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
